9d68540a470beca15a7eac31a8d12a16fdfe9845
Christina Mastro <christinamastro@Christinas-MacBook-Pro.local>
2014-11-04 12:13:14 -0500

    regenerated devise views and added picture uploading

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	  # Prevent CSRF attacks by raising an exception.
+	  # For APIs, you may want to use :null_session instead.
+	  protect_from_forgery with: :exception
+
+	  before_filter :configure_devise_params, if: :devise_controller?
+
+    def configure_devise_params
+		devise_parameter_sanitizer.for(:sign_up) do |u|
+  			u.permit(:email, :password, :password_confirmation, :picture)
+		end
+	end	
 end
